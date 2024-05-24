e0091d60e67ca68256d8291a79c8f00aad6e7da8
Dag-Erling Smørgrav <des@des.dev>
2024-05-24 13:40:44 +0200

    Standalone test for bound bug.

---
+++ tests/axy.c
@@ -0,0 +1,33 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stddef.h>
+#include <regex.h>
+
+int
+main(void)
+{
+	const char *re;
+	regex_t reg;
+
+#if 0
+	re = "^([a-z]([a-z-]{0,3}[a-z])?\\.)*[a-z]{1,3}$";
+	if (regcomp(&reg, re, REG_EXTENDED | REG_NOSUB) != 0 ||
+	    regexec(&reg, "dns", 0, NULL, 0) != 0 ||
+	    regexec(&reg, "dns.dns", 0, NULL, 0) != 0 ||
+	    regexec(&reg, "dnsdns.dns", 0, NULL, 0) != REG_NOMATCH)
+		return 1;
+#else
+	re = "^((a{1,3})?x)*y";
+	if (regcomp(&reg, re, REG_EXTENDED | REG_NOSUB) != 0 ||
+//	    regexec(&reg, "xy", 0, NULL, 0) != 0 ||
+//	    regexec(&reg, "axy", 0, NULL, 0) != 0 ||
+//	    regexec(&reg, "aaxy", 0, NULL, 0) != 0 ||
+//	    regexec(&reg, "aaaxy", 0, NULL, 0) != 0 ||
+	    regexec(&reg, "aaaaxy", 0, NULL, 0) != REG_NOMATCH)
+		return 1;
+#endif
+	regfree(&reg);
+	return 0;
+}
